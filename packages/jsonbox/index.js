d02e21e07a119dcdf12cef8f5d6cc31bbe4fc556
Israel Roldan <israel.roldan@sencha.com>
2017-04-26 15:28:09 +0200

    Added support for jsonbox.fromPropertiesFile

--- packages/jsonbox/index.js
+++ packages/jsonbox/index.js
@@ -1,1 +1,46 @@
-module.exports = require('./lib/jsonbox');
+const assignment = require('assignment');
+const escapeStringRegexp = require('escape-string-regexp');
+const flatten = require('flat');
+const unflatten = flatten.unflatten;
+
+class Jsonbox {
+    constructor (obj) {
+        this._data = obj;
+    }
+
+    set (obj) {
+        assignment(this._data, obj);
+        return this;
+    }
+
+    remove (keys) {
+        let me = this;
+        me._data = flatten(me._data);
+        keys.forEach((r) => {
+            let regex = new RegExp(`${escapeStringRegexp(r)}(\\.|$)`);
+            Object.keys(me._data).forEach((k) => {
+                if (regex.test(k)) {
+                    delete me._data[k];
+                }
+            });
+        });
+        me._data = unflatten(me._data);
+        return this;
+    }
+
+    build () {
+        return this._data;
+    }
+
+    toString (indent = 2) {
+        return JSON.stringify(this._data, null, indent);
+    }
+
+    // -----------------------------------------
+
+    static fromPropertiesFile (path) {
+        return new Jsonbox(unflatten(require('properties-parser').read(path)));
+    }
+}
+
+module.exports = Jsonbox;
--- packages/jsonbox/test/playground.js
+++ packages/jsonbox/test/playground.js
@@ -1,3 +1,4 @@
 const jsonbox = require('..');
 
 console.log(new jsonbox({foo: "bar"}).set({'baz':'abc'}).toString());
+console.log(jsonbox.fromPropertiesFile('./fixture/foo.properties').toString());
